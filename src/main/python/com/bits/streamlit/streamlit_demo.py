7b20cd1aaa294b49abf30dfe0350065f9f57a3e7
Rajiv <rajiv.kumar.102@gmail.com>
2024-09-30 19:24:34 +0530

    Integrated UI and model

--- src/main/python/com/bits/streamlit/streamlit_demo.py
+++ src/main/python/com/bits/streamlit/streamlit_demo.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pickle
 
 # Run code streamlit run streamlit_demo.py
+# Be at ai-ml dir
+# streamlit run .\src\main\python\com\bits\streamlit\streamlit_demo.py
+# streamlit run .\src\main\python\com\bits\streamlit\streamlit_demo.py
 # http://localhost:8501/
+
+with open('models/random_for_streamlit_model.pkl', 'rb') as f :
+    model = pickle.load(f);
+
 st.title("My Streamlit Application")
 number = st.slider("Pick a number", 0, 100)
 sl = st.slider("Sepal Length(cm)", min_value=4.0, max_value=8.0, value=5.0, step=0.1)
@@ -11,7 +19,7 @@ pw = st.slider("Petal width(cm)", min_value=4.0, max_value=8.0, value=5.0, step=
 
 if st.button('Predict'):
     input_data = [[sl,pl,sw,pw]]
-    #pridict = model.
-    pridiction = 'Versicolur'
+    pridiction =  model.predict(input_data)
+    #pridiction = 'Versicolur'
 
     st.write(f"The pridicted species is: {pridiction}")
